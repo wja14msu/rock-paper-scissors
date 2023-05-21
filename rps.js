de13e29542ee3066b3162b9997a078f10ba91ad2
Wander5 <wja14msu@gmail.com>
2023-05-20 21:54:08 -0500

    working on buttons

--- rps.js
+++ rps.js
@@ -1,3 +1,9 @@
+let roundCount = 0;
+let winCount = 0;
+let loseCount = 0;
+let tieCount = 0;
+let result = ''
+
 function getComputerChoice() {
     let array = ["rock", "paper", "scissors"];
     let computerChoice = array[Math.floor(Math.random() * array.length)];
@@ -5,9 +11,55 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-
+    if (playerSelection === "rock" && computerSelection === "rock") {
+        console.log("tie");
+        result = "tie";
+        return tieCount++;
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+        console.log("lose");
+        result = "lose";
+        return loseCount++;
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        console.log("win");
+        result = "win";
+        return winCount++;
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        console.log("win");
+        result = "win";
+        return winCount++;
+    } else if (playerSelection === "paper" && computerSelection === "paper") {
+        console.log("tie");
+        result = "tie";
+        return tieCount++;
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        console.log("lose");
+        result = "lose";
+        return loseCount++;
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        console.log("lose");
+        result = "lose";
+        return loseCount++;
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        console.log("win");
+        result = "win";
+        return winCount++;
+    } else if (playerSelection === "scissors" && computerSelection === "scissors") {
+        console.log("tie");
+        result = "tie";
+        return tieCount++;
+    }
+    result = document.getElementById("result");
+    result.innerHTML = result;
+    return [result, winCount, loseCount, tieCount];
+    console.log(winCount, loseCount, tieCount);
 }
 
-const playerSelection = "rock";
+//let playerSelection = prompt();
+//playerSelection = playerSelection.toLowerCase();
 const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+//console.log(playRound(playerSelection, computerSelection));
+//console.log("player: ", playerSelection, "computer: ", computerSelection);
+
+let playerSelectionRock = "rock";
+let playerSelectionPaper = "paper";
+let playerSelectionScissors = "scissors";
